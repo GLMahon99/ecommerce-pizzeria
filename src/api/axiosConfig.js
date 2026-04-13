70e88e3a4fcb53f063d60642d98ebbd93981fa68
GLMahon99 <gastimahon@gmail.com>
2026-04-13 17:39:46 -0300

    agrego s a la solicitud http

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://pizzeria-ecommerce-production.up.railway.app/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://pizzeria-ecommerce-production.up.railway.app/api',
 });
 
 export default api;
